227367fe8a48a38b3aa57346152e5b11e9f6d992
Nipponvisions <30487560+nipponvisions@users.noreply.github.com>
2018-11-08 23:52:08 -0500

    Update page.php

--- page.php
+++ page.php
@@ -11,58 +11,56 @@ description: The template for displaying pages
 
 <div class="notice"> this is page.php </div>
 
-   <div class="below_header"> 
 
 
-   	
 		<?php 
 			if ( have_posts() ) {
 				while ( have_posts() ) {
 					the_post(); 
-					
-
-					if(is_page('portfolio')){
-						// add disclaimer box
-						//hide template files from Wordpress' Appearance Editor
-						// https://wordpress.stackexchange.com/questions/223479/hide-certain-template-files-in-apearance-editor/223494#223494
-						 include (get_template_directory() . '/template-parts/page/disclaimer_box.php');
-					}
 					?>
+					<div class="title">  <h3> <?php  the_title(); ?> </h3> 	 </div> 
 
-					<h3> <?php  the_title(); ?>	</h3>
+					<div class= "two_columns">
+						<ul>
+							<li> 
+							<?php
 
-						<div class="three_columns">
-							<ul class="three_columns">
-								<li> <?php get_sidebar(); ?> </li>
+							// set post_thumbnails according to pages
+							if (has_post_thumbnail()) {
+								# code...
+								if(is_page()){
+									the_post_thumbnail('banner_image');
+								}
+							}
+							?>
 
-								<li> 	<?php
-									// set post_thumbnails according to pages
-									if (has_post_thumbnail()) {
-										# code...
-										if(is_page()){
-											the_post_thumbnail('banner_image');
-										}
-									}
+							</li>
 
-									?>
-					 			</li>
+							<li>
+								<?php  include (get_template_directory() . '/template-parts/page/customfield_template.php');  ?>
 
-								<li>  	<?php  include (get_template_directory() . '/template-parts/page/customfield_template.php');  ?> </li> 
+							</li>
 
-							</ul><!--three_columns --> 
-						</div><!--three_columns -->
+						</ul>
 
-					 	<div class="content">
+						<div class="content">
 							<?php the_content(); ?> 
 
-						</div><!-- content -->
+						</div>
+					</div>
+
+
+					
+					
+
 
 	
 				<?php		
 					} // end while
 				} // end if
 		?>
-	</div><!-- below_header -->
+
+	   	<?php get_sidebar(); ?>
 
    <?php get_footer() ; ?>
 
