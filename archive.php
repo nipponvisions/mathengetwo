669d9809c529d368bae6f22f2dcf0cad428882e9
Nipponvisions <30487560+nipponvisions@users.noreply.github.com>
2018-11-08 23:59:02 -0500

    Update archive.php

--- archive.php
+++ archive.php
@@ -11,55 +11,61 @@ description:  for displaying the posts archives
 
 <div class="notice"> this is archive-girl.php </div>
 
-    <div class="below_header"> 
+	
 
 		<?php 
-		if ( have_posts() ) {
-			while ( have_posts() ) {
-				the_post();
-				//
-				?>
+			if ( have_posts() ) {
+				while ( have_posts() ) {
+					the_post(); 
+					?>
+		
 
-				<h3> <?php  the_title(); ?> </h3>
+					<div class= "two_columns">
+						<ul>
 
-				<!-- 	two_columns  -->
-				<div class="two_columns">
-					<ul>
-						<li>
-							<div class= "post_thumbnail">
-								<?php
 
-								// check if the post has a Post Thumbnail assigned to it.
-								if ( has_post_thumbnail()) {
-									the_post_thumbnail('thumbnail');
-								}else{
-									// display a place holder image 
 
-									?>
-									<img src="<?php bloginfo('template_url')?>/assets/images/image_place_holder.png" style="width:150px; height:150px;">
+							<li> 
 
-									<?php
-								}
-										
-								?>
-
-							</div>
-						</li>
-
-						<li>
-							<div class="excerpt">
 							
-								<?php the_excerpt(); ?> <a href="<?php echo get_permalink(); ?>"> Read More...</a>
-							 </div>
-					
-						</li>
-					</ul>
-				</div>
+								<ul>
+									<li>  
+										<div class= "post_thumbnail">
+											<?php
+											// check if the post has a Post Thumbnail assigned to it.
+											if ( has_post_thumbnail()) {
+												the_post_thumbnail('thumbnail');
+											}else{
+												// display a place holder image 
+												?>
+												<img src="<?php bloginfo('template_url')?>/assets/images/image_place_holder.png" style="width:150px; height:150px;">
 
-			<?php		
-			} // end while
-		} // end if
+												<?php
+											}
+													
+											?>
+										</div><!--post_thumbnail -->
+
+									</li>
+
+									<li style="max-width:60%;"> 
+										<div class="excerpt">
+											 <?php the_excerpt(); ?> <a href="<?php echo get_permalink(); ?>"> Read More...</a> 
+
+										</div>
+									</li>
+								</ul>
+							</li>
+						</ul>
+					</div><!-- two_columns --> 
+
+				<?php		
+					} // end while
+				} // end if
 		?>
-	</div><!-- below_header -->
 
-<?php get_footer() ; ?>
+	   	<?php get_sidebar(); ?>
+
+   <?php get_footer() ; ?>
+
+
